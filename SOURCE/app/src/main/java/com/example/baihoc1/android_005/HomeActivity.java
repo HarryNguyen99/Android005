057fa935090e276c212887e189446d337c39e2b9
Harry Nguyen <phuquyit@gmail.com>
2019-05-28 14:30:37 +0700

    share

--- SOURCE/app/src/main/java/com/example/baihoc1/android_005/HomeActivity.java
+++ SOURCE/app/src/main/java/com/example/baihoc1/android_005/HomeActivity.java
@@ -8,6 +8,6 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_layout);
+        setContentView(R.layout.activity_home);
     }
 }
